41977168e02ce26b08d615dce4b6ffe1b24eb680
zaynegoering <zaynegoering@gmail.com>
2020-12-01 15:15:15 -0800

    commit tree 741673abf97044fedb7f92d1b0c1487330be7df1

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
+require_relative 'models/pig_latinizer'
+require "pry"
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        # binding.pry
+        @latinized_word = PigLatinizer.new.piglatinize(params[:user_phrase])
+        erb :pig_latinize
+    end
+
 end
